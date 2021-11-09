24a9a1b94b0ecf08a583a1560911da88adb71574
caitlin-ohara <caitlinohara@microsoft.com>
2021-11-09 12:10:38 +0000

    Add project files.

---
+++ contozoosecondf/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace contozoosecondf.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
